2066c05638bc2456fa78be9955481b9769a09d20
SamerSiam <ssiam05@gmail.com>
2021-12-24 00:18:18 +0200

    functionality done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
+export class App extends Component {
+ 
+  inputRef=React.createRef();
+state={yes:0, no:0, value:0}
 
-    <div className="App">
-      hi 
-         
-    </div>
-  );
+yes=() =>{
+  this.setState({yes:this.state.yes+1});
 }
 
-export default App;
+no=() =>{
+  this.setState({no:this.state.no+1});
+}
+
+getImageUrl = (index) => {
+  return `https://picsum.photos/500/500?random=${index}`;
+  }
+
+  componentDidUpdate(){
+    this.getImageUrl()
+  }
+  render() {
+
+    if (this.state.yes+ this.state.no <= this.state.value)
+    {
+
+      return (
+          <div className="App-container">
+      
+        <img  src={this.getImageUrl(Date.now())} alt="random"/>
+        <div >
+        Enter how many pictures you like to see:
+          <input ref={this.inputRef} className="ui field" type="text" value={this.state.value} 
+                     onChange={e=> this.setState({value:e.target.value })}/>
+       
+        </div>
+        
+       <div className="button">
+        <button className="button" onClick={this.yes}>Yes </button>
+        <button className="button" onClick={this.no}>No </button>
+      </div>
+      <div className="button">
+       <div> {this.state.yes} </div>
+       <div> {this.state.no}</div> 
+      </div>
+       
+
+      </div>
+    )
+    }
+    else{
+      return (
+
+        <div> Game Over</div>
+      )
+    }
+  }
+}
+
+export default App
